bb4e2d6aa7487d6d425261a9c46805ae3ea286ef
Сумаренко Екатерина Михайловна <sumarenka@sc.softclub.by>
2022-10-17 18:03:15 +0300

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PreviewComponent } from './preview/preview.component';
 import { PaginatorCarouselComponent } from './preview/paginator-carousel/paginator-carousel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PaginatorCarouselComponent } from './preview/paginator-carousel/paginat
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
